13bb796c48b9278a0f2d9c6c6583568e2638459c
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-23 17:32:57 +1200

    Refactor: Remove loading indicator from ImageCellThumbnail
    The loading indicator ("…") in `ImageCellThumbnail` has been removed. The thumbnail will now only display the image once the bitmap is available.

--- app/src/main/java/denys/diomaxius/assignment_two/MainActivity.kt
+++ app/src/main/java/denys/diomaxius/assignment_two/MainActivity.kt
@@ -1,15 +1,18 @@
 package denys.diomaxius.assignment_two
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import dagger.hilt.android.AndroidEntryPoint
 import denys.diomaxius.assignment_two.ui.screen.GalleryScreen
 import denys.diomaxius.assignment_two.ui.theme.Assignment_twoTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
