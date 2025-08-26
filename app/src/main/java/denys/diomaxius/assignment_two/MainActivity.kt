c358f96500b9bd1da9e6711dc8c09892e2375ed0
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-26 19:12:38 +1200

    Refactor: Remove unnecessary API requirement and add onResume override
    This commit removes the `@RequiresApi(Build.VERSION_CODES.TIRAMISU)` annotation from the `onCreate` method in `MainActivity.kt` as it was not necessary.
    
    Additionally, an empty `onResume` method override has been added to `MainActivity.kt`.

--- app/src/main/java/denys/diomaxius/assignment_two/MainActivity.kt
+++ app/src/main/java/denys/diomaxius/assignment_two/MainActivity.kt
@@ -1,18 +1,15 @@
 package denys.diomaxius.assignment_two
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import dagger.hilt.android.AndroidEntryPoint
 import denys.diomaxius.assignment_two.ui.screen.GalleryScreen
 import denys.diomaxius.assignment_two.ui.theme.Assignment_twoTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,5 +18,10 @@ class MainActivity : ComponentActivity() {
                 GalleryScreen()
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
     }
 }
