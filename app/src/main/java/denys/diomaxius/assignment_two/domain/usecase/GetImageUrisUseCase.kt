e3400362667171209f809aa3ed589b83ee51b1cd
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-08-26 19:08:02 +1200

    Refactor: Simplify GetImageUrisUseCase and remove unnecessary line

--- app/src/main/java/denys/diomaxius/assignment_two/domain/usecase/GetImageUrisUseCase.kt
+++ app/src/main/java/denys/diomaxius/assignment_two/domain/usecase/GetImageUrisUseCase.kt
@@ -1,6 +1,5 @@
 package denys.diomaxius.assignment_two.domain.usecase
 
-import android.util.Log
 import denys.diomaxius.assignment_two.domain.model.ImageItem
 import denys.diomaxius.assignment_two.domain.repository.ImageRepository
 import javax.inject.Inject
@@ -8,10 +7,6 @@ import javax.inject.Inject
 class GetImageUrisUseCase @Inject constructor(
     private val imageRepository: ImageRepository,
 ) {
-    suspend operator fun invoke(): List<ImageItem> {
-        val uris = imageRepository.loadImageUris()
-        Log.i("Check uris", "$uris")
-        return uris
-    }
-
+    suspend operator fun invoke(): List<ImageItem> =
+        imageRepository.loadImageUris()
 }
